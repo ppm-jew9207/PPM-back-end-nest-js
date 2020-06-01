628b5657657712b0e01a36088bb2934aa24eb4be
auris auris <aurimas.butk@gmail.com>
2020-06-01 12:26:55 +0100

    create mentors module,controller,service

--- src/app/domains/domains.module.ts
+++ src/app/domains/domains.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ProjectsDomainModule } from './projects/projects.module';
 import { ProjectCreated } from './projects/events/project-created.event';
+import { MentorsModule } from './mentors/mentors.module';
 
 @Module({
-  imports: [CqrsModule, ProjectsDomainModule],
+  imports: [CqrsModule, ProjectsDomainModule, MentorsModule],
   controllers: [],
   providers: [
     ProjectCreated
