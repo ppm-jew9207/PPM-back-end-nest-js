b14d041cc4e1b8084a3de22b08128e010729ccc8
auris auris <aurimas.butk@gmail.com>
2020-06-10 18:44:44 +0100

    review fix

--- apps/api/src/app/domains/mentors/mentors.controller.ts
+++ apps/api/src/app/domains/mentors/mentors.controller.ts
@@ -2,13 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateMentorPayloadDto } from './dto/create-mentor-payload.dto';
 import { CreateMentor } from './commands/create-mentor.command';
-import { MentorsService } from './mentors.service';
 
 @Controller('mentors')
 export class MentorsController {
   constructor(
     private readonly _commandBus: CommandBus,
-    private readonly _mentorsService: MentorsService,
   ) { }
 
   @Post()
--- apps/api/src/app/domains/mentors/mentors.service.ts
+++ apps/api/src/app/domains/mentors/mentors.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class MentorsService{
-     a =[]
-}
+export class MentorsService{}
