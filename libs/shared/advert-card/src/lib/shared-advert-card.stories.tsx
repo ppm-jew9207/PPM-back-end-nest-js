a28cd1192ab24897f64e93218017a1f6a443e565
MariuzM <mariuzz7789@gmail.com>
2020-06-11 20:13:09 +0100

    created-advert-card

---
+++ libs/shared/advert-card/src/lib/shared-advert-card.stories.tsx
@@ -0,0 +1,25 @@
+import { text, , number, text, number, number, text } from '@storybook/addon-knobs';
+import React from 'react';
+import SharedAdvertCard, {  SharedAdvertCardProps  } from './shared-advert-card';
+
+export default {
+  component: SharedAdvertCard,
+  title: 'SharedAdvertCard'
+};
+
+export const primary = () => {
+  
+  
+  const props:SharedAdvertCardProps = {
+    title: text('title', ''),
+    author: ('author', ),
+    timeStamp: number('timeStamp', 0),
+    description: text('description', ''),
+    like: number('like', 0),
+    shared: number('shared', 0),
+    imgUrl: text('imgUrl', ''),
+  };
+  
+
+  return <SharedAdvertCard title = {props.title} author = {props.author} timeStamp = {props.timeStamp} description = {props.description} like = {props.like} shared = {props.shared} imgUrl = {props.imgUrl}  />;
+};
