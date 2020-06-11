7a2f7acaee3167a0fc44d016f1975364fef23e73
Gardemarin <gintaras.kuskys@gmail.com>
2020-06-11 17:59:16 +0100

    fix(view-adverts-service):[skp-100] Removes unnecessary data spread.

--- apps/api/src/app/models/adverts/adverts.service.ts
+++ apps/api/src/app/models/adverts/adverts.service.ts
@@ -4,11 +4,18 @@ import { Model } from 'mongoose';
 import { Types } from 'mongoose';
 
 import { ViewModels } from '../../helpers/constants';
-import { AdvertsViewModel, CreateAdvertPayload, RemoveAdvertPayload, UpdateAdvertPayload } from './adverts.interface';
+import {
+  AdvertsViewModel,
+  CreateAdvertPayload,
+  RemoveAdvertPayload,
+  UpdateAdvertPayload,
+} from './adverts.interface';
 
 @Injectable()
-export class AdvertsModelService  {
-  @InjectModel(ViewModels.ADVERTS_VIEW) private _model!: Model<AdvertsViewModel>;
+export class AdvertsModelService {
+  @InjectModel(ViewModels.ADVERTS_VIEW) private _model!: Model<
+    AdvertsViewModel
+  >;
 
   async getAll(): Promise<AdvertsViewModel[]> {
     return this._model.find().exec();
@@ -19,24 +26,20 @@ export class AdvertsModelService  {
   }
 
   async create(id: string, data: CreateAdvertPayload) {
-    await this._model.findOneAndUpdate(
-      { _id: Types.ObjectId(id) },
-      { ...data },
-      { upsert: true, new: true },
-    );
+    await this._model.findOneAndUpdate({ _id: Types.ObjectId(id) }, data, {
+      upsert: true,
+      new: true,
+    });
   }
 
   async update(id: string, data: UpdateAdvertPayload) {
-    await this._model.findOneAndUpdate(
-      { _id: Types.ObjectId(id) },
-      { ...data },
-      { upsert: true, new: true },
-    );
+    await this._model.findOneAndUpdate({ _id: Types.ObjectId(id) }, data, {
+      upsert: true,
+      new: true,
+    });
   }
 
   async remove({ id }: RemoveAdvertPayload) {
-    await this._model.deleteOne(
-      { _id: Types.ObjectId(id) }
-    );
-  }  
+    await this._model.deleteOne({ _id: Types.ObjectId(id) });
+  }
 }
