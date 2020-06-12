1fb97d35b77bc4e5eea7a7099ae9258294d45fe0
MariuzM <mariuzz7789@gmail.com>
2020-06-12 19:49:54 +0100

    updated my comp storybook

--- libs/shared/storybook/.storybook/webpack.config.js
+++ libs/shared/storybook/.storybook/webpack.config.js
@@ -2,7 +2,7 @@ const rootWebpackConfig = require('../../../../.storybook/webpack.config');
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
   config = await rootWebpackConfig({ config, mode });
-  
+
   config.resolve.extensions.push('.tsx');
   config.resolve.extensions.push('.ts');
   config.module.rules.push({
@@ -12,9 +12,9 @@ module.exports = async ({ config, mode }) => {
       presets: [
         '@babel/preset-env',
         '@babel/preset-react',
-        '@babel/preset-typescript'
-      ]
-    }
+        '@babel/preset-typescript',
+      ],
+    },
   });
   config.module.rules.push({
     test: /\.scss$/,
