5f735f59f5480596aa72b3edd31afb0fbacc3e44
MariuzM <mariuzz7789@gmail.com>
2020-06-13 17:31:35 +0100

    adding time

--- libs/shared/advert-card/src/lib/shared-advert-card.tsx
+++ libs/shared/advert-card/src/lib/shared-advert-card.tsx
@@ -23,6 +23,61 @@ export interface SharedAdvertCardProps {
   onSharedClick?: () => void;
 }
 
+function addZero(i) {
+  if (i < 10) {
+    i = '0' + i;
+  }
+  return i;
+}
+
+function timeCalculator() {
+  const dbDate = '2020-06-13 16:21:10';
+  // const yyyy: number = +dbDate.slice(0, 4);
+  // const mm: number = +dbDate.slice(5, 7);
+  // const dd: number = +dbDate.slice(8, 10);
+  const date = dbDate.slice(0, 10);
+  // const h: number = +dbDate.slice(11, 13);
+  // const m: number = +dbDate.slice(14, 16);
+  // const s: number = +dbDate.slice(17, 19);
+  const time = dbDate.slice(11, 19);
+  // console.log('dbDate - time', yyyy, mm, dd, ' ', h, m, s);
+  console.log('dbDate - time', date, time);
+
+  // const startTime: any = new Date(`${date}T16:00:10`);
+  // const startTime: any = new Date(`2020-06-13T16:00:10`);
+  const startTime: any = new Date('2020-06-13T14:00:10');
+  console.log('Start Time', startTime);
+  console.log(startTime.getMilliseconds());
+
+  const d = new Date();
+
+  console.log(d.getHours(), d.getMinutes(), d.getSeconds());
+
+  // const date2 = new Date();
+  // console.log(
+  //   date2.getFullYear() + '-' + (date2.getMonth() + 1) + '-' + date2.getDate()
+  // );
+  // const today = new Date();
+  // const dd = today.;
+
+  // const mm = today.getMonth() + 1;
+  // const yyyy = today.getFullYear();
+
+  // const endTime: any = new Date(`${yyyy}:${mm}:${dd}T16:02:00`);
+  // const endTime: any = new Date('2020-06-13T16:02:00');
+  const endTime: any = new Date();
+  console.log('endTime', endTime);
+  const timeDiff = (endTime - startTime) / 1000;
+
+  let totalSeconds = Math.round(timeDiff);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds %= 3600) / 60);
+  const seconds = totalSeconds % 60;
+  console.log(`${hours} hour, ${minutes} minute and ${seconds} seconds`);
+}
+
+timeCalculator();
+
 export const SharedAdvertCard = (props: SharedAdvertCardProps) => {
   return (
     <div className="advert-card">
