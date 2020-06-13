4cf3869f30d0b439928ae06a1d71d167aa4e713f
Donatas Kuskys <donatas.kuskys@gmail.com>
2020-06-13 22:35:30 +0300

    api port changes for deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -51,8 +51,15 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.port || process.env.PORT;
-  const host = process.env.host || process.env.HOST;
+  const port = process.env.PORT || process.env.port;
+  const host = process.env.HOST || process.env.host;
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
   await app.listen(port, () => {
     console.log(
       'Listening at http://' + host + ':' + port + '/' + globalPrefix
