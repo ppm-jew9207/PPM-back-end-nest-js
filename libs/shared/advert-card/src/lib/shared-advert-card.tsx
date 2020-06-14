08d9a67c5be483ecd4410b99de0ec3f62f2b2b9f
MariuzM <mariuzz7789@gmail.com>
2020-06-14 22:57:34 +0100

    added months date/time

--- libs/shared/advert-card/src/lib/shared-advert-card.tsx
+++ libs/shared/advert-card/src/lib/shared-advert-card.tsx
@@ -38,16 +38,27 @@ export const SharedAdvertCard = (props: SharedAdvertCardProps) => {
   const classes = useStyles();
 
   function timeCalculator() {
-    const then = moment('2020-06-13 22:00:00');
+    const then = moment('2020-04-14 22:00:00');
     const now = moment('2020-06-14 22:10:01');
     const duration = moment.duration(now.diff(then));
+
+    const months = duration.months();
+    console.log(months);
 
     const days = duration.days();
     const hours = duration.hours();
     const minutes = duration.minutes();
     const seconds = duration.seconds();
 
-    if (days > 0) {
+    if (months > 0) {
+      setTimeState(`
+				${months} month${months > 1 ? 's' : ''},
+				${days > 0 ? `${days} day${days > 1 ? 's' : ''},` : ''}
+				${hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''},` : ''}
+				${minutes > 0 ? `${minutes} minute${minutes > 1 ? 's' : ''} and ` : ''}
+				${seconds > 0 ? `${seconds} second${seconds > 1 ? 's' : ''}` : ''}
+			`);
+    } else if (days > 0) {
       setTimeState(`
 				${days} day${days > 1 ? 's' : ''},
 				${hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''},` : ''}
