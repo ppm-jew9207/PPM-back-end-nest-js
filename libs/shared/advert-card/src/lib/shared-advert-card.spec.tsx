a6bd4869e9df14b0571d1ddafd384dd497008529
MariuzM <mariuzz7789@gmail.com>
2020-06-14 23:52:05 +0100

    fixed merge from master

--- libs/shared/advert-card/src/lib/shared-advert-card.spec.tsx
+++ libs/shared/advert-card/src/lib/shared-advert-card.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import SharedAdvertCard from './shared-advert-card';
+import { SharedAdvertCard } from './shared-advert-card';
 
 describe(' SharedAdvertCard', () => {
   it('should render successfully', () => {
