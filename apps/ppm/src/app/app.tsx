942dc6630566e6546c4f5b4b4b3901b8d2d8be37
MariuzM <mariuzz7789@gmail.com>
2020-06-16 16:46:24 +0100

    final

--- apps/ppm/src/app/app.tsx
+++ apps/ppm/src/app/app.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import { SharedAdvertCard } from '@ppm/shared/advert-card';
+
 import './app.scss';
 
-const authorObj = {
-  _id: '1',
-  firstName: 'Marius',
-  lastName: 'Mariakinas',
-  img:
-    'https://cdn.thecoachingmanual.com/front_end_technical_test_assets/avatar_1.jpg',
-};
-
-const App = () => {
-  return (
-    <SharedAdvertCard
-      title="Title"
-      author={authorObj}
-      timeStamp={'2020-06-14 21:24:00'}
-      description="This is Description"
-      like={3}
-      shared={5}
-      imgUrl="https://images.unsplash.com/photo-1591268557714-1b58f58e89cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80"
-    />
-  );
+export const App = () => {
+  return <div></div>;
 };
 
 export default App;
