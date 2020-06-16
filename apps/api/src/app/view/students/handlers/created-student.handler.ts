045578d5d348d9e6ae1c166a299111e5d14a5cfc
auris auris <aurimas.butk@gmail.com>
2020-06-16 21:03:05 +0100

    save student data in mongo db

---
+++ apps/api/src/app/view/students/handlers/created-student.handler.ts
@@ -0,0 +1,14 @@
+import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
+import { MentorCreated } from '../../../domains/mentors/events/mentor-created.event';
+import { Inject } from '@nestjs/common';
+import { StudentCreated } from '../../../domains/students/events/student-created.event';
+import { StudentModelService } from '../../../models/students/student.service';
+
+@EventsHandler(StudentCreated)
+export class StudentCreatedHandler implements IEventHandler<StudentCreated>{
+    @Inject() private readonly _studentModelService: StudentModelService;
+    public async handle(event: StudentCreated) {
+        await this._studentModelService.create(event)
+        return null;
+    }
+}
