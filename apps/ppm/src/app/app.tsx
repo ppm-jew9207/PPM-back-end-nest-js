6f99d3b952c93bf6cdee7d0cf2205b2795a490fb
MariuzM <mariuzz7789@gmail.com>
2020-06-17 23:11:48 +0100

    removed test data

--- apps/ppm/src/app/app.tsx
+++ apps/ppm/src/app/app.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { SharedInfoCard } from '@ppm/shared/info-card';
+
 import './app.scss';
 
 export const App = () => {
-  return (
-    <SharedInfoCard
-      title="Title"
-      fields={[
-        { label: 'Label1', value: 'Value1' },
-        { label: 'Label2', value: 'Value2' },
-      ]}
-    />
-  );
+  return <div></div>;
 };
 
 export default App;
