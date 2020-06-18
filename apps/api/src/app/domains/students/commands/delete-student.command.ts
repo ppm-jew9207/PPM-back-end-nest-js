e7616b9b960d8ad20ba6c8859c20b3bc7996b7f2
auris auris <aurimas.butk@gmail.com>
2020-06-18 19:16:22 +0100

    review fix - update command name

--- apps/api/src/app/domains/students/commands/delete-student.command.ts
+++ apps/api/src/app/domains/students/commands/delete-student.command.ts
@@ -3,14 +3,14 @@ import { Inject } from '@nestjs/common';
 import { StudentAggregate } from '../students.aggregate';
 import { StudentDeleted } from '../events/student-deleted.event';
 
-export class DeleteStudentId {
+export class DeleteStudentCommand {
     constructor(public id: string) {
     }
 };
-@CommandHandler(DeleteStudentId)
-export class DeleteStudentHandler implements ICommandHandler<DeleteStudentId> {
+@CommandHandler(DeleteStudentCommand)
+export class DeleteStudentHandler implements ICommandHandler<DeleteStudentCommand> {
     @Inject() private readonly _publisher: EventPublisher;
-    async execute({ id }: DeleteStudentId) {
+    async execute({ id }: DeleteStudentCommand) {
         const aggregate = new StudentAggregate();
         aggregate.apply(new StudentDeleted(id));
 
--- apps/api/src/app/domains/students/events/student-deleted.event.ts
+++ apps/api/src/app/domains/students/events/student-deleted.event.ts
@@ -1,5 +1,3 @@
-import { DeleteStudentId } from '../commands/delete-student.command';
-
 export class StudentDeleted {
     constructor(public readonly student: string) {
     }
