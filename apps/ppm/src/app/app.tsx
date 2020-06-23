a1e6ca761c0109fde98cefe96ca0e396896335b5
MariuzM <mariuzz7789@gmail.com>
2020-06-23 20:26:44 +0100

    final fix

--- apps/ppm/src/app/app.tsx
+++ apps/ppm/src/app/app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { SharedTopRightBar } from '@ppm/shared/top-right-bar';
+
 import './app.scss';
 
 export const App = () => {
-  return <SharedTopRightBar />;
+  return <div></div>;
 };
 
 export default App;
