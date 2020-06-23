140dc45a38682d5f607a0b8c0bda38cc6d64fec1
auris auris <aurimas.butk@gmail.com>
2020-06-23 19:00:10 +0100

    fix conflict

--- apps/api/src/app/view/students/students.controller.ts
+++ apps/api/src/app/view/students/students.controller.ts
@@ -4,14 +4,10 @@ import { AuthGuard } from '@nestjs/passport';
 import { LoggingInterceptor } from '../../common/interceptors/logging.interceptor';
 import { TransformInterceptor } from '../../common/interceptors/transform.interceptor';
 import { QueryBus } from '@nestjs/cqrs';
-<<<<<<< HEAD
-import { GetStudentsQuery } from './queries/get-students.handlers';
-import { StudentPayloadDto } from '../../models/students/dto/student-payload.dto';
-=======
 import { CreateStudentPayloadDto } from '../../models/students/dto/create-student-payload.dto';
 import { GetStudentByIdQuery } from './queries/get-student-by-id.handler';
-
->>>>>>> 6bf5dc364baef02fb8b763e393e1ff7297a6eacb
+import { StudentPayloadDto } from '../../models/students/dto/student-payload.dto';
+import { GetStudentsQuery } from './queries/get-students.handlers';
 
 @Controller('students-view')
 @ApiBearerAuth('JWT')
@@ -19,15 +15,13 @@ import { GetStudentByIdQuery } from './queries/get-student-by-id.handler';
 @UseInterceptors(LoggingInterceptor, TransformInterceptor)
 export class StudentsController {
     constructor(private readonly queryBus: QueryBus) { }
-<<<<<<< HEAD
 
     @Get()
     async findAll(): Promise<StudentPayloadDto[]> {
         return this.queryBus.execute(new GetStudentsQuery())
-=======
+    }
     @Get('/:id')
     async getById(@Param('id') id: string): Promise<CreateStudentPayloadDto> {
         return this.queryBus.execute(new GetStudentByIdQuery(id));
->>>>>>> 6bf5dc364baef02fb8b763e393e1ff7297a6eacb
     }
 }
