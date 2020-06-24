ab1f08e8bd96671da2c2bbcb24ccd28b937f7c0a
Lukas Ružauskas <nequadora@gmail.com>
2020-06-24 08:53:00 +0300

    storybook created

--- apps/ppm/src/app/app.tsx
+++ apps/ppm/src/app/app.tsx
@@ -1,43 +1,9 @@
 import React from 'react';
 
 import './app.scss';
-import { SharedNavigation } from '@ppm/shared/navigation';
-const callName = (id: string): void => {
-  console.log(id);
-};
 
-const buttons = [
-  {
-    label: 'Home',
-    path: '/',
-    icon: 'home',
-    onClick: () => callName('home1'),
-  },
-  {
-    label: 'Home2',
-    path: '/home2',
-    icon: 'apps',
-    onClick: () => callName('home2'),
-  },
-  {
-    label: 'Home3',
-    path: '/home3',
-    icon: 'homeWork',
-    onClick: () => callName('home3'),
-  },
-  {
-    label: 'Home4',
-    path: '/home4',
-    icon: 'home',
-    onClick: () => callName('home4'),
-  },
-];
 export const App = () => {
-  return (
-    <div>
-      <SharedNavigation buttons={buttons} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
