98d271f987e605e6860a6c7e5c530b1158613d79
auris auris <aurimas.butk@gmail.com>
2020-06-25 17:28:05 +0100

    fix conflict

--- apps/api/src/app/view/students/students.module.ts
+++ apps/api/src/app/view/students/students.module.ts
@@ -4,10 +4,6 @@ import { EventHandlers } from './handlers';
 import { StudentModelModule } from '../../models/students/student.module';
 import { QueryHandlers } from './queries';
 import { StudentsController } from './students.controller';
-<<<<<<< HEAD
-
-=======
->>>>>>> 6bf5dc364baef02fb8b763e393e1ff7297a6eacb
 
 @Module({
   imports: [CqrsModule, StudentModelModule],
