ddd4fcfcc3eb5459b1e089a5831367e2c1a81204
Gardemarin <gintaras.kuskys@gmail.com>
2020-06-29 18:54:37 +0100

    feat(mentors-sagas):[skp-127] Creates Mentors Saga with Permissions

--- apps/api/src/app/sagas/mentors.sagas.ts
+++ apps/api/src/app/sagas/mentors.sagas.ts
@@ -1,8 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ICommand, ofType, Saga, CommandBus } from '@nestjs/cqrs';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { CreateMentor } from '../domains/mentors/commands/create-mentor.command';
+import { switchMap, concatAll } from 'rxjs/operators';
 import { UsersService } from '../models/users/users.service';
 import { MentorCreated } from '../domains/mentors/events/mentor-created.event';
 import { CreatePermission } from '../domains/permissions/commands/create-permission.command';
@@ -12,21 +11,25 @@ export class MentorsSagas {
   @Inject() private readonly _userService: UsersService;
 
   @Saga()
-  createMentor = (
-    events$: Observable<MentorCreated>
-  ): Observable<ICommand[]> => {
+  createMentor = (events$: Observable<MentorCreated>): Observable<ICommand> => {
     return events$.pipe(
       ofType(MentorCreated),
       switchMap(async (event) => {
         const user = await this._userService.getById(event.userId);
-
-        return user.roles.map((role) => {
-          return new CreatePermission({
-            role,
-            user: { _id: user._id, name: user.userName },
-          });
-        });
-      })
+        return Promise.all(
+          user.roles.map(async (role) => {
+            if (role) {
+              const cmd = new CreatePermission({
+                role,
+                user: { _id: user._id, name: user.userName },
+              });
+              return cmd;
+            }
+            return null;
+          })
+        );
+      }),
+      concatAll()
     );
   };
 }
