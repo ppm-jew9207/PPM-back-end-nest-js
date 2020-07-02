b7385d8094da42bfa720da939b851ba685f93e9f
Gardemarin <gintaras.kuskys@gmail.com>
2020-07-02 18:59:14 +0100

    feat(students):skp-138 Students Story owner review fix

--- apps/api/src/app/domains/students/students.controller.ts
+++ apps/api/src/app/domains/students/students.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, Param, Logger, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateStudent } from './commands/create-student.command';
 import { CreateStudentPayloadDto } from '../../models/students/dto/create-student-payload.dto';
@@ -9,28 +16,45 @@ import { LoggingInterceptor } from '../../common/interceptors/logging.intercepto
 import { TransformInterceptor } from '../../common/interceptors/transform.interceptor';
 import { UpdateStudentPayloadDto } from '../../models/students/dto/update-student-payload.dto';
 import { UpdateStudentCommand } from './commands/update-student.command';
+import { Types } from 'mongoose';
+import { ResponseError } from '../../common/dto/response.dto';
 
 @Controller('students')
 @ApiBearerAuth('JWT')
 @UseGuards(AuthGuard('jwt'))
 @UseInterceptors(LoggingInterceptor, TransformInterceptor)
 export class StudentsController {
-    constructor(
-        private readonly _commandBus: CommandBus,
-    ) { }
+  constructor(private readonly _commandBus: CommandBus) {}
 
-    @Post()
-    async create(@Body() payload: CreateStudentPayloadDto) {
-        return this._commandBus.execute(new CreateStudent(payload))
-    }
+  @Post()
+  async create(@Body() payload: CreateStudentPayloadDto) {
+    return this._commandBus.execute(new CreateStudent(payload));
+  }
 
-    @Post(':id/delete')
-    async delete(@Param('id') id: string) {
-        return this._commandBus.execute(new DeleteStudentCommand(id))
+  @Post(':id/delete')
+  async delete(@Param('id') id: string) {
+    try {
+      if (!Types.ObjectId.isValid(id)) {
+        return new ResponseError('DATA.ERROR', new Error('Error: Invalid Id'));
+      }
+    } catch (error) {
+      return new ResponseError('DATA.ERROR', error);
     }
+    return this._commandBus.execute(new DeleteStudentCommand(id));
+  }
 
-    @Post(':id/update')
-    async update(@Param('id') id: string, @Body() payload: UpdateStudentPayloadDto) {
-        return this._commandBus.execute(new UpdateStudentCommand(id, payload))
+  @Post(':id/update')
+  async update(
+    @Param('id') id: string,
+    @Body() payload: UpdateStudentPayloadDto
+  ) {
+    try {
+      if (!Types.ObjectId.isValid(id)) {
+        return new ResponseError('DATA.ERROR', new Error('Error: Invalid Id'));
+      }
+    } catch (error) {
+      return new ResponseError('DATA.ERROR', error);
     }
+    return this._commandBus.execute(new UpdateStudentCommand(id, payload));
+  }
 }
