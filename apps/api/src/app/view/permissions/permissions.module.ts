5918d913af4ed9dc7005b0b88b72429e9a1bbb07
Donatas Kuskys <donatas.kuskys@gmail.com>
2020-07-03 01:00:37 +0300

    removed permissions controllers

--- apps/api/src/app/view/permissions/permissions.module.ts
+++ apps/api/src/app/view/permissions/permissions.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { EventHandlers } from './handlers';
-import { PermissionsController } from './permissions.controller';
 import { PermissionsModelModule } from '../../models/permissions/permissions.module';
 
 @Module({
   imports: [CqrsModule, PermissionsModelModule],
-  controllers: [PermissionsController],
   providers: [...EventHandlers],
 })
 export class PermissionsViewModule {}
