587fa62b3fb324763e48d3d468928708bd88af13
Gardemarin <gintaras.kuskys@gmail.com>
2020-07-07 19:25:08 +0100

    Optional change to required.

--- apps/api/src/app/models/userProfiles/dto/create-user-profile-payload.dto.ts
+++ apps/api/src/app/models/userProfiles/dto/create-user-profile-payload.dto.ts
@@ -3,15 +3,15 @@ import { CreateUserProfile } from '../user-profile.interface';
 
 export class CreateUserProfilePayloadDto implements CreateUserProfile {
   @ApiProperty()
-  public readonly firstName?: string;
+  public readonly firstName: string;
   @ApiProperty()
-  public readonly lastName?: string;
+  public readonly lastName: string;
   @ApiProperty()
-  public readonly photo?: string;
+  public readonly photo: string;
   @ApiProperty()
-  public readonly fieldOfProfession?: string;
+  public readonly fieldOfProfession: string;
   @ApiProperty({ isArray: true, type: 'string' })
-  public readonly categories?: string[];
+  public readonly categories: string[];
   @ApiProperty()
-  public readonly city?: string;
+  public readonly city: string;
 }
