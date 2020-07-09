7823094af38f3733ca108ac6be51a2e31262d9d6
Lukas Ružauskas <nequadora@gmail.com>
2020-07-09 22:35:32 +0300

    added new shared search input component

---
+++ libs/shared/search-input/src/lib/shared-search-input.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useMemo } from 'react';
+import TextField from '@material-ui/core/TextField';
+
+import Grid from '@material-ui/core/Grid/Grid';
+import SearchIcon from '@material-ui/icons/Search';
+import { debounce } from 'lodash';
+
+import './shared-search-input.scss';
+
+export interface SharedSearchInputProps {
+  onSearch: (code: string) => void;
+}
+
+export const SharedSearchInput = (props: SharedSearchInputProps) => {
+  const [inputText, setInputText] = useState('');
+
+  const waitTime = useMemo(
+    () =>
+      debounce((searchString: string) => {
+        props.onSearch(searchString);
+      }, 500),
+    []
+  );
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+    waitTime(e.target.value);
+  };
+
+  return (
+    <Grid container spacing={1} alignItems="flex-end">
+      <Grid item>
+        <SearchIcon />
+      </Grid>
+      <Grid item>
+        <TextField label="Search" onChange={handleChange} value={inputText} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SharedSearchInput;
