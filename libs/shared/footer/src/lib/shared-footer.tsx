c022ef667d7c37886938a250bb1cf7fdee3e4cb8
Karolis Račas <karolis.racas@gmail.com>
2020-07-20 20:22:53 +0300

    create footer empty component

---
+++ libs/shared/footer/src/lib/shared-footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './shared-footer.scss';
+
+/* eslint-disable-next-line */
+export interface SharedFooterProps {}
+
+export const SharedFooter = (props: SharedFooterProps) => {
+  return (
+    <div>
+      <h1>Welcome to shared-footer!</h1>
+    </div>
+  );
+};
+
+export default SharedFooter;
