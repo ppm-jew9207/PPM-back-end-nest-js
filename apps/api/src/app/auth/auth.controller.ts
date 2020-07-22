7ff6d1f5eb720cdac2c4bdd65468e4910252632e
Donatas Kuskys <donatas.kuskys@gmail.com>
2020-07-22 08:14:49 +0300

    removed console log

--- apps/api/src/app/auth/auth.controller.ts
+++ apps/api/src/app/auth/auth.controller.ts
@@ -29,9 +29,7 @@ export class AuthController {
   @Post(PrivateRoutesPath.POST_LOGIN)
   @HttpCode(HttpStatus.OK)
   public async login(@Body() login: LoginDto): Promise<IResponse> {
-    try {
-      console.log(login);
-      
+    try {      
       const response = await this.authService.validateLogin(
         login.email,
         login.password
