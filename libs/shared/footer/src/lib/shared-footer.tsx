48c34465662400e477f406ef4c90f6dc2620282b
Karolis Račas <karolis.racas@gmail.com>
2020-07-23 21:44:28 +0300

    add material-ui grid layout

--- libs/shared/footer/src/lib/shared-footer.tsx
+++ libs/shared/footer/src/lib/shared-footer.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 
 import './shared-footer.scss';
-import AppBar from '@material-ui/core/AppBar';
-import Container from '@material-ui/core/Container';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Grid,
+  Paper } from '@material-ui/core';
 
 /* eslint-disable-next-line */
 export interface SharedFooterProps {}
 
 export const SharedFooter = (props: SharedFooterProps) => {
   return (
-    <AppBar position="static" color="primary">
+    <AppBar className="footer-bar" position="static" color="primary">
+        <Grid container>
+          <Grid item xs={3}>
+            <p>Material UI</p>
+          </Grid>
+          <Grid item xs={3}>
+            <p>Community</p>
+            <p>Community</p>
+            <p>Community</p>
+            <p>Community</p>
+          </Grid>
+          <Grid item xs={3}>
+            <p>Resources</p>
+            <p>Resources</p>
+            <p>Resources</p>
+          </Grid>
+          <Grid item xs={3}>
+            <p>Company</p>
+            <p>Company</p>
+            <p>Company</p>
+          </Grid>
+        </Grid>
+
         <Container maxWidth="75%">
             <Toolbar>
               <Typography variant="body1" color="inherit">
