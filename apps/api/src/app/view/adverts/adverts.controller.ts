75cf1e937aceaa869a786ac8efa0b2e2ba1c32ea
Gardemarin <gintaras.kuskys@gmail.com>
2020-07-28 09:48:19 +0100

    fix(view-adverts): [skp-214] Removes auth from GET Adverts query

--- apps/api/src/app/view/adverts/adverts.controller.ts
+++ apps/api/src/app/view/adverts/adverts.controller.ts
@@ -1,5 +1,5 @@
-import {  Controller, Get, Param, UseGuards, UseInterceptors } from '@nestjs/common';
-import {  QueryBus } from '@nestjs/cqrs';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
 import { GetAdvertsQuery } from './queries/handlers/get-adverts.handler';
 import { GetAdvertQuery } from './queries/handlers/get-advert.handler';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
@@ -11,12 +11,9 @@ import { PrivateRoutesPath } from '@ppm/common/main';
 @Controller(PrivateRoutesPath.ADVERTS)
 @ApiTags(PrivateRoutesPath.ADVERTS)
 @ApiBearerAuth('JWT')
-@UseGuards(AuthGuard('jwt'))
 @UseInterceptors(LoggingInterceptor, TransformInterceptor)
 export class AdvertsController {
-  constructor(
-    private readonly queryBus: QueryBus,
-  ) {}
+  constructor(private readonly queryBus: QueryBus) {}
 
   @Get()
   async findAll(): Promise<any[]> {
