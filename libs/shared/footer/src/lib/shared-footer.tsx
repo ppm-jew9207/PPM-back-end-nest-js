a0ca2aa4fb06d0aced95c32c7394fbf8ef42605e
Karolis Račas <karolis.racas@gmail.com>
2020-07-28 21:22:57 +0300

    refactor code

--- libs/shared/footer/src/lib/shared-footer.tsx
+++ libs/shared/footer/src/lib/shared-footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './shared-footer.scss';
 import HomeIcon from '@material-ui/icons/Home';
@@ -8,98 +8,73 @@ import {
   Grid,
   Link,
   Typography,
-  Paper } from '@material-ui/core';
-
+  Paper,
+} from '@material-ui/core';
 
 /* eslint-disable-next-line */
 export interface SharedFooterProps {}
 
 export const SharedFooter = (props: SharedFooterProps) => {
+  const [menu, setMenu] = useState({
+    menu1: {
+      allMenu: [
+        { tittle: 'link1', path: '#' },
+        { tittle: 'link2', path: '#' },
+        { tittle: 'link3', path: '#' },
+      ],
+      tiitle: 'Title 1',
+    },
+    menu2: {
+      allMenu: [
+        { tittle: 'link1', path: '#' },
+        { tittle: 'link2', path: '#' },
+        { tittle: 'link3', path: '#' },
+      ],
+      tiitle: 'Title 2',
+    },
+    menu3: {
+      allMenu: [
+        { tittle: 'link1', path: '#' },
+        { tittle: 'link2', path: '#' },
+        { tittle: 'link3', path: '#' },
+      ],
+      tiitle: 'Title 3',
+    },
+  });
   return (
-    <Paper className="footer" elevation={3} >
-        <Container maxWidth="75%">
-          <Grid container>
+    <Paper className="footer" elevation={3}>
+      <Container maxWidth="75%">
+        <Grid container>
           <Grid item xs={3}>
             <Box>
               <Typography>
                 <Link className="footer-link" href="#">
-                  <HomeIcon/> PPM
+                  <HomeIcon /> PPM
                 </Link>
               </Typography>
             </Box>
           </Grid>
-          <Grid item xs={3}>
-            <Typography variant="body1" color="textPrimary">
-              Title 1
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 1
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 2
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 3
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 4
-              </Link>
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="body1" color="textPrimary">
-              Title 2
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 1
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 2
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 3
-              </Link>
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="body1" color="textPrimary">
-              Title 3
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 1
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 2
-              </Link>
-            </Typography>
-            <Typography>
-              <Link className="footer-link" href="#">
-                Link 3
-              </Link>
-            </Typography>
-          </Grid>
+          {Object.keys(menu).map((menuKey, menuIndex) => (
+            <Grid key={menuIndex} item xs={3}>
+              <Typography variant="body1" color="textPrimary">
+                Title 1
+              </Typography>
+              {menu[menuKey].allMenu.map((item, index) => (
+                <Typography key={index}>
+                  <Link className="footer-link" href={item.path}>
+                    {item.tittle}
+                  </Link>
+                </Typography>
+              ))}
+            </Grid>
+          ))}
         </Grid>
-        </Container>
-        <Container maxWidth="75%">
-          <Typography variant="body1" color="inherit">
-            &copy; 2020 PPM
-          </Typography>
-        </Container>
+      </Container>
+      <Container maxWidth="75%">
+        <Typography variant="body1" color="inherit">
+          &copy; 2020 PPM
+        </Typography>
+      </Container>
     </Paper>
   );
 };
