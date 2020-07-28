4571e1efee3e8049d040cb1a2e6a9339557bd56a
Gardemarin <gintaras.kuskys@gmail.com>
2020-07-28 19:59:31 +0100

    (feat-shared):[skp-198] Creates basic Create Advert form

---
+++ libs/shared/create-advert-form/src/lib/shared-create-advert-form.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { TextField, Button, Box, Typography } from '@material-ui/core';
+import './shared-create-advert-form.scss';
+
+export interface SharedForgotPasswordFormProps {
+  onSubmit: (advertData: { title: string }) => void;
+  title: string;
+  subtitle: string;
+  submitButtonText: string;
+  inputLabel: string;
+}
+
+export interface SharedCreateAdvertFormProps {
+  onSubmit: (advertData: { title: string }) => void;
+  title: string;
+  submitButtonText: string;
+  titleInputLabel: string;
+  descriptionInputLabel: string;
+}
+
+export const SharedCreateAdvertForm = (props: SharedCreateAdvertFormProps) => {
+  const { handleSubmit, register, control, errors } = useForm();
+
+  return (
+    <Box maxWidth={500} display="flex" flexDirection="column" mx="auto">
+      <Typography className="header" component="h1" variant="h5">
+        {props.title}
+      </Typography>
+
+      <form autoComplete="off" onSubmit={handleSubmit(props.onSubmit)}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="title"
+          label={props.titleInputLabel}
+          type="text"
+          name="title"
+          autoComplete="title"
+          autoFocus
+          inputRef={register({
+            required: 'Required',
+          })}
+          error={!!errors.title}
+          helperText={!errors.title ? '' : 'Title is required'}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          id="description"
+          label={props.descriptionInputLabel}
+          type="text"
+          name="description"
+          autoComplete="description"
+          autoFocus
+          inputRef={register({})}
+          multiline
+          rows={8}
+        />
+        <Button fullWidth variant="contained" color="primary" type="submit">
+          {props.submitButtonText}
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default SharedCreateAdvertForm;
