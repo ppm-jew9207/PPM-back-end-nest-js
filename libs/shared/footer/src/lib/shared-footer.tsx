8d25a1ed4083d49bbe85e5ce4a1f2b0a73dddb0a
Karolis Račas <karolis.racas@gmail.com>
2020-07-29 20:20:06 +0300

    adjust footer styles

--- libs/shared/footer/src/lib/shared-footer.tsx
+++ libs/shared/footer/src/lib/shared-footer.tsx
@@ -39,9 +39,9 @@ export const SharedFooter = (props: SharedFooterProps) => {
         <Grid container>
           <Grid item xs={3}>
             <Box>
-              <Typography>
-                <Link className="footer-link" href="#">
-                  <HomeIcon /> PPM
+              <Typography variant="body1">
+                <Link className="footer-home" color="textPrimary" href="#">
+                  <HomeIcon className="footer-icon" /> PPM
                 </Link>
               </Typography>
             </Box>
@@ -49,14 +49,18 @@ export const SharedFooter = (props: SharedFooterProps) => {
           {!!menu &&
             Object.keys(menu).map((menuKey, menuIndex) => (
               <Grid key={menuIndex} item xs={3}>
-                <Typography variant="body1" color="textPrimary">
+                <Typography className="footer-title" variant="body1">
                   {menu[menuKey].title}
                 </Typography>
                 {!!menu[menuKey] &&
                   !!menu[menuKey].allMenu &&
                   menu[menuKey].allMenu.map((item, index) => (
-                    <Typography key={index}>
-                      <Link className="footer-link" href={item.path}>
+                    <Typography variant="body2" key={index}>
+                      <Link
+                        className="footer-link"
+                        color="textSecondary"
+                        href={item.path}
+                      >
                         {item.tittle}
                       </Link>
                     </Typography>
@@ -65,9 +69,13 @@ export const SharedFooter = (props: SharedFooterProps) => {
             ))}
         </Grid>
       </Container>
-      <Container maxWidth="75%">
-        <Typography variant="body1" color="inherit">
-          &copy; 2020 PPM
+      <Container maxWidth="xl">
+        <Typography
+          className="footer-copy"
+          variant="body2"
+          color="textSecondary"
+        >
+          Copyright &copy; 2020 PPM
         </Typography>
       </Container>
     </Paper>
