1c99951114c0f8fe714aa63bb44c554924ddc255
Lukas Ružauskas <nequadora@gmail.com>
2020-08-18 22:33:50 +0300

    added user reducer and action to get profile

--- libs/data-access/redux-configuration/src/lib/injectedReducers.ts
+++ libs/data-access/redux-configuration/src/lib/injectedReducers.ts
@@ -1,9 +1,11 @@
 import { authorizeReducer } from '@ppm/data-access/authorization';
 import { advertsListReducer } from '@ppm/data-access/adverts-list';
 import { advertReducer } from '@ppm/data-access/advert';
+import { userProfileReducer } from '@ppm/data-access/user-profile';
 
 export const allReducers = {
   authorization: authorizeReducer,
   advertsList: advertsListReducer,
   advert: advertReducer,
+  userProfile: userProfileReducer,
 };
