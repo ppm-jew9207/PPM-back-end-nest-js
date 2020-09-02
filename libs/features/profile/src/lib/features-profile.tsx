4d5de6559859b7eb45e506dfbb92e23b4caccf40
Karolis Račas <karolis.racas@gmail.com>
2020-09-02 19:16:13 +0300

    create feature with test data

--- libs/features/profile/src/lib/features-profile.tsx
+++ libs/features/profile/src/lib/features-profile.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SharedUserProfileCard } from '@ppm/shared/user-profile-card';
+import { useSelector, useDispatch } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { createStructuredSelector } from 'reselect';
+import {
+  userProfileActions,
+  userProfileSelectors,
+} from '@ppm/data-access/user-profile';
+
 import './features-profile.scss';
 
-/* eslint-disable-next-line */
-export interface FeaturesProfileProps {}
+const stateSelector = createStructuredSelector({
+  profiles: userProfileSelectors.selectUserProfile(),
+  loading: userProfileSelectors.selectLoading(),
+});
 
-export const FeaturesProfile = (props: FeaturesProfileProps) => {
+export const FeaturesProfile = (props) => {
+  const dispatch = useDispatch();
+  const { profiles, loading } = useSelector(stateSelector);
+
+  useEffect(() => {
+    dispatch(userProfileActions.getUserProfile(props.match.params.id));
+  }, [dispatch]);
+
+  const data = {
+    mentorImage: 'https://placekitten.com/300/300',
+    fullName: 'John Doe',
+    aboutMentor: 'Lorem ipsum dolor sit amet',
+    mentorLocation: 'Vilnius',
+    facebook: '#',
+    twitter: '#',
+    linkedin: '#',
+    instagram: '#',
+  };
+
+  if (loading) return <CircularProgress />;
+
   return (
     <div>
-      <SharedUserProfileCard />
+      <SharedUserProfileCard {...data} />
     </div>
   );
 };
