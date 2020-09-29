2e1eabaf105a7ac6f86a3b1a9ba8517397c9cb75
Donatas Kuskys <donatas.kuskys@gmail.com>
2020-09-29 22:50:49 +0300

    fixed

--- libs/features/registration/src/lib/features-registration.tsx
+++ libs/features/registration/src/lib/features-registration.tsx
@@ -5,9 +5,11 @@ import StepLabel from '@material-ui/core/StepLabel';
 import './features-registration.scss';
 import { CircularProgress } from '@material-ui/core';
 import { useRegistration } from '@ppm/hooks/use-registration';
-
-export const FeaturesRegistration = (props: {history: object}) => {
-  const {loading, steps, registrationStep, getStepContent} = useRegistration(props.history);
+import {
+  History
+} from '@ppm/common/main';
+export const FeaturesRegistration = (props: {history: History}) => {
+  const {loading, staticSteps, registrationStep, getStepContent} = useRegistration(props.history);
 
   return (
     <div className="features-registration">
@@ -19,7 +21,7 @@ export const FeaturesRegistration = (props: {history: object}) => {
         </div>
       }
       <Stepper activeStep={registrationStep} alternativeLabel>
-        {steps.map((label) => (
+        {staticSteps.map((label) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
           </Step>
