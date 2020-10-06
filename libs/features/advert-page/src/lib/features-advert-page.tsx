b7fd3bf898ed87518ee13c1dde27db08c260cba5
Lukas Ružauskas <nequadora@gmail.com>
2020-10-06 21:53:43 +0300

    created component

---
+++ libs/features/advert-page/src/lib/features-advert-page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './features-advert-page.scss';
+
+/* eslint-disable-next-line */
+export interface FeaturesAdvertPageProps {}
+
+export const FeaturesAdvertPage = (props: FeaturesAdvertPageProps) => {
+  return (
+    <div>
+      <h1>Welcome to features-advert-page!</h1>
+    </div>
+  );
+};
+
+export default FeaturesAdvertPage;
