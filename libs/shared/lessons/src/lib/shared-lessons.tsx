b241efc0520e29234100e2299620e0970e40992b
Donatas Kuskys <donatas.kuskys@gmail.com>
2020-10-06 15:34:43 +0300

    fixed changes

--- libs/shared/lessons/src/lib/shared-lessons.tsx
+++ libs/shared/lessons/src/lib/shared-lessons.tsx
@@ -30,7 +30,7 @@ export interface Action {
   action?(lesson: Lesson): void;
 }
 
-const LessonItem = (lesson: Lesson, actions: Action[]) => {
+const LessonItem = (props: {lesson: Lesson, actions: Action[]}) => {
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
     null
   );
@@ -45,38 +45,38 @@ const LessonItem = (lesson: Lesson, actions: Action[]) => {
 
   const open = Boolean(anchorEl);
 
-  const preventDefault = (event: React.SyntheticEvent) =>
-    event.preventDefault();
+  const preventDefault = (event: React.SyntheticEvent) =>{ event.preventDefault();}
 
   return (
+    <div>
     <ListItem className="lesson" alignItems="flex-start">
       <div className="lesson__details">
         <Link className="lesson__title" href="#" onClick={preventDefault}>
-          <h3>{lesson.title}</h3>
+          <h3>{props.lesson.title}</h3>
         </Link>
 
         <address className="author lesson__text">
           By
           <a rel="author" href="#">
-            {lesson.creator.name}
+            {props.lesson.creator.name}
           </a>
         </address>
-        <time className=" lesson__text" dateTime={lesson.datime}>
-          {new Date(lesson.datime).toDateString()}
+        <time className=" lesson__text" dateTime={props.lesson.datime}>
+          {new Date(props.lesson.datime).toDateString()}
         </time>
         <span className=" lesson__text">2h30min</span>
       </div>
-      <img className="lesson__image" src={lesson.imageUrl} />
+      <img className="lesson__image" src={props.lesson.imageUrl} />
       <Button
         variant="text"
         color="primary"
-        aria-describedby={lesson._id}
+        aria-describedby={props.lesson._id}
         onClick={handleClick}
       >
         <Icon className="fas fa-plus-circle" color="secondary" />
       </Button>
       <Popover
-        id={lesson._id}
+        id={props.lesson._id}
         open={open}
         onClose={handleClose}
         anchorEl={anchorEl}
@@ -105,6 +105,7 @@ const LessonItem = (lesson: Lesson, actions: Action[]) => {
         </ButtonGroup>
       </Popover>
     </ListItem>
+    </div>
   );
 };
 
@@ -117,11 +118,11 @@ export interface SharedLessonsProps {
 export const SharedLessons = (props: SharedLessonsProps) => {
   return (
     <List className="lessons">
-      {props.lessons.map((lesson) => {
-        <div key={lesson._id}>
+      {props.lessons.map((lesson) => 
+        (<div key={lesson._id}>
           <LessonItem lesson={lesson} actions={props.actions} />
-        </div>;
-      })}
+        </div>)
+      )}
     </List>
   );
 };
