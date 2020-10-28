0e49d0fdc27b878f002419197d925652f3cf3538
Lukas Ružauskas <nequadora@gmail.com>
2020-10-28 21:19:02 +0200

    reduced limit amount

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100000, // limit each IP to 100 requests per windowMs
+      max: 100, // limit each IP to 100 requests per windowMs
       message: 'Too many requests from this IP, please try again later'
     })
   );
