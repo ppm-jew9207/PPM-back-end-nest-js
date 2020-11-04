0cfe2ee52b61ce9508ed9fae290fdece28ace2a7
Lukas Ružauskas <nequadora@gmail.com>
2020-11-04 20:24:31 +0200

    test for hooks use adverts

--- libs/hooks/use-adverts/src/lib/hooks-use-adverts.spec.tsx
+++ libs/hooks/use-adverts/src/lib/hooks-use-adverts.spec.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import HooksUseAdverts from './hooks-use-adverts';
+import { useAdverts } from './hooks-use-adverts';
 
-describe('HooksUseAdverts', () => {
+const mockSelectors = jest.mock('react-redux', () => ({
+  advert: jest.fn(),
+  loading: jest.fn(),
+}));
+
+jest.mock('react-redux', () => ({
+  useDispatch: () => jest.fn(),
+  useSelector: () => jest.fn(() => mockSelectors),
+}));
+
+describe('useAdverts', () => {
+  let result;
   it('should render successfully', () => {
-    const { baseElement } = render(<HooksUseAdverts />);
-    expect(baseElement).toBeTruthy();
+    function HookRunner() {
+      result = useAdverts(1);
+      return null;
+    }
+    render(<HookRunner />);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('useRegistration should return objects', () => {
+    expect(result).toEqual({
+      advert: undefined,
+      getAdvertById: result.getAdvertById,
+      loading: undefined,
+      onGetStartedClick: result.onGetStartedClick,
+    });
   });
 });
