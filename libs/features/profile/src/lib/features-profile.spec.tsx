337c520b0939942bbd9719e3643be2a77d1a7c01
Donatas <donatas.kuskys@gmail.com>
2020-11-11 23:19:36 +0200

    created profile test

--- libs/features/profile/src/lib/features-profile.spec.tsx
+++ libs/features/profile/src/lib/features-profile.spec.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import configureStore from 'redux-mock-store';
+import * as redux from 'react-redux';
 import FeaturesProfile from './features-profile';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
 
 describe(' FeaturesProfile', () => {
+  let storeMock;
+  let component;
+  const configureStoreMock = configureStore();
+  storeMock = configureStoreMock(
+    {
+      profile: {photo: ''},
+      loading: false,
+      adverts: [{_id: 'tetsId', creator: {_id: '4jh35b23jh5', name: 'Jon', imageUrl: 'http://google.lt.paveikliukas.jpg' }}],
+    }
+  );
+
+  beforeEach(() => {
+    jest.spyOn(redux, 'useSelector').mockImplementation(() => storeMock.getState());
+    jest.spyOn(redux, 'useDispatch').mockImplementation(() => storeMock.dispatch);
+    component = shallow(<FeaturesProfile  store={storeMock}/>);
+
+  });
   it('should render successfully', () => {
-    const { baseElement } = render(<FeaturesProfile />);
-    expect(baseElement).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
