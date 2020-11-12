0319cf83ad24810b6cc31deef1aca503f772faf2
Lukas Ružauskas <nequadora@gmail.com>
2020-11-12 21:06:44 +0200

    100% coverage of features-advert-page

--- libs/features/advert-page/src/lib/features-advert-page.spec.tsx
+++ libs/features/advert-page/src/lib/features-advert-page.spec.tsx
@@ -1,28 +1,81 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import configureStore from 'redux-mock-store';
+import * as redux from 'react-redux';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import FeaturesAdvertPage from './features-advert-page';
 
-jest.mock('', () => ({
-  advert: {
-    title: 'TitleName',
-    description: 'That is wonderful lesson',
-    creator: {
-      name: 'Lukas Ruzauskas',
-      id: '1111111111111111',
-    },
-    imageUrl: 'https://via.placeholder.com/150',
-    category: 'Javascript',
-    programmingLanguages: ['Javascript', 'Typescript'],
-    preRequisites: 'University degree',
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('react-router-dom', () => ({
+  useParams: () => {
+    return '1111111111';
   },
-  loading: false,
-  onGetStartedClick: () => jest.fn(),
 }));
 
 describe('FeaturesAdvertPage', () => {
+  let storeMock;
+  let component;
+  const configureStoreMock = configureStore();
+
   it('should render successfully', () => {
-    const result = render(<FeaturesAdvertPage />);
-    expect(result).toBeTruthy();
+    storeMock = configureStoreMock({
+      advert: {
+        title: 'TitleName',
+        description: 'That is wonderful lesson',
+        creator: {
+          name: 'Lukas Ruzauskas',
+          id: '1111111111111111',
+        },
+        imageUrl: 'https://via.placeholder.com/150',
+        category: 'Javascript',
+        programmingLanguages: ['Javascript', 'Typescript'],
+        preRequisites: 'University degree',
+        lessons: [],
+      },
+      loading: false,
+    });
+
+    jest
+      .spyOn(redux, 'useSelector')
+      .mockImplementation(() => storeMock.getState());
+    jest
+      .spyOn(redux, 'useDispatch')
+      .mockImplementation(() => storeMock.dispatch);
+    component = shallow(<FeaturesAdvertPage store={storeMock} />);
+
+    expect(component).toBeTruthy();
+  });
+
+  it('should render error successfully', () => {
+    storeMock = configureStoreMock({
+      advert: null,
+      loading: false,
+    });
+
+    jest
+      .spyOn(redux, 'useSelector')
+      .mockImplementation(() => storeMock.getState());
+    jest
+      .spyOn(redux, 'useDispatch')
+      .mockImplementation(() => storeMock.dispatch);
+    component = shallow(<FeaturesAdvertPage store={storeMock} />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should load loading', () => {
+    storeMock = configureStoreMock({
+      advert: null,
+      loading: true,
+    });
+
+    jest
+      .spyOn(redux, 'useSelector')
+      .mockImplementation(() => storeMock.getState());
+    jest
+      .spyOn(redux, 'useDispatch')
+      .mockImplementation(() => storeMock.dispatch);
+    component = shallow(<FeaturesAdvertPage store={storeMock} />);
+    expect(component).toBeTruthy();
   });
 });
