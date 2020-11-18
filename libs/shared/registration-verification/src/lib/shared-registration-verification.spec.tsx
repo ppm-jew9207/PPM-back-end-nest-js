d59c15bd4a14b2d7f7a26d00886d3a9a543f4e9c
Karolis Račas <karolis.racas@gmail.com>
2020-11-18 20:20:50 +0200

    fix test

--- libs/shared/registration-verification/src/lib/shared-registration-verification.spec.tsx
+++ libs/shared/registration-verification/src/lib/shared-registration-verification.spec.tsx
@@ -3,9 +3,19 @@ import { render } from '@testing-library/react';
 
 import SharedRegistrationVerification from './shared-registration-verification';
 
+const code = 'Input text';
+const informationTitle = 'A verification code has been sent to your registration email';
+
 describe(' SharedRegistrationVerification', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<SharedRegistrationVerification />);
-    expect(baseElement).toBeTruthy();
+    const result = render(
+    <SharedRegistrationVerification
+      code={code}
+      informationTitle={informationTitle}
+      onSubmit={(e) => console.log(e)}
+      onCancel={() => console.log('void')}
+      resendVerification={() => console.log('void')}
+    />);
+    expect(result).toBeTruthy();
   });
 });
