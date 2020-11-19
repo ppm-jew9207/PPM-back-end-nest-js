eb937b6b8315c79d2ead3cafe1ef9865174461dc
Karolis Račas <karolis.racas@gmail.com>
2020-11-19 23:27:58 +0200

    fix shared-top-right-bar test

--- libs/shared/top-right-bar/src/lib/shared-top-right-bar.spec.tsx
+++ libs/shared/top-right-bar/src/lib/shared-top-right-bar.spec.tsx
@@ -3,9 +3,35 @@ import { render } from '@testing-library/react';
 
 import SharedTopRightBar from './shared-top-right-bar';
 
+const DataFromDB = {
+  notifications: [
+    {
+      _id: 'sef65e56fgwe6g',
+      title: 'Notification 1',
+      path: '/somepath1',
+      timeStamp: '2020-02-03',
+      isRead: false,
+    },
+    {
+      _id: 'sef65e56fgwe6g22',
+      title: 'Notification 2',
+      path: '/somepath2',
+      timeStamp: '2020-02-03',
+      isRead: false,
+    },
+  ],
+  settings: { _id: '65ef5w6e4fw56', label: 'Some label', path: '/settings' },
+  profile: { _id: '65ef5w6e4fw56', label: 'Marius', path: '/user-profile' },
+};
+
 describe(' SharedTopRightBar', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<SharedTopRightBar />);
-    expect(baseElement).toBeTruthy();
+    const result = render(
+      <SharedTopRightBar
+      notifications={DataFromDB.notifications}
+      settings={DataFromDB.settings}
+      profile={DataFromDB.profile}
+    />);
+    expect(result).toBeTruthy();
   });
 });
