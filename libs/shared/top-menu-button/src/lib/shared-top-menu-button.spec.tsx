8f2715431e487c7ae4463816b6dc745742812f1a
Karolis Račas <karolis.racas@gmail.com>
2020-11-19 23:32:12 +0200

    fix shared-top-menu-button

--- libs/shared/top-menu-button/src/lib/shared-top-menu-button.spec.tsx
+++ libs/shared/top-menu-button/src/lib/shared-top-menu-button.spec.tsx
@@ -3,9 +3,22 @@ import { render } from '@testing-library/react';
 
 import SharedTopMenuButton from './shared-top-menu-button';
 
+const items = [
+  {
+    _id: '',
+    title: 'Title',
+    path: '',
+    timeStamp: '',
+    isRead: true,
+  }
+];
+
 describe(' SharedTopMenuButton', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<SharedTopMenuButton />);
-    expect(baseElement).toBeTruthy();
+    const result = render(
+    <SharedTopMenuButton 
+      items={items}
+    />);
+    expect(result).toBeTruthy();
   });
 });
