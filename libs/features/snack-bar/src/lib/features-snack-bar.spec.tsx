e0b0107eeb8ac7755fe6edf6fe48538bf47bc0d7
Karolis Račas <karolis.racas@gmail.com>
2020-11-29 17:39:32 +0200

    mock notistack

--- libs/features/snack-bar/src/lib/features-snack-bar.spec.tsx
+++ libs/features/snack-bar/src/lib/features-snack-bar.spec.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import configureStore from 'redux-mock-store';
+import * as redux from 'react-redux';
 import FeaturesSnackBar from './features-snack-bar';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const mockEnqueue = jest.fn();
+
+jest.mock('notistack', () => ({
+...jest.requireActual('notistack'),
+useSnackbar: () => {
+  return {
+    enqueueSnackbar: mockEnqueue
+  };
+}
+}));
 
 describe(' FeaturesSnackBar', () => {
+  let storeMock;
+  let component;
+  const configureStoreMock = configureStore();
+  storeMock = configureStoreMock(
+    {
+      message: null,
+      type: null
+    }
+  );
+
+  beforeEach(() => {
+    jest.spyOn(redux, 'useSelector').mockImplementation(() => storeMock.getState());
+    jest.spyOn(redux, 'useDispatch').mockImplementation(() => storeMock.dispatch);
+    component = shallow(<FeaturesSnackBar store={storeMock}/>);
+
+  });
   it('should render successfully', () => {
-    const { baseElement } = render(<FeaturesSnackBar />);
-    expect(baseElement).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
