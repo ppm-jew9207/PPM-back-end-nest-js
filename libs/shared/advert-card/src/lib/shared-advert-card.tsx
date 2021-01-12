e3486ccfd7095ceb463548a00c2a657b3c96f6bb
Lukas Ružauskas <nequadora@gmail.com>
2021-01-12 12:11:15 +0200

    editing state and styling

--- libs/shared/advert-card/src/lib/shared-advert-card.tsx
+++ libs/shared/advert-card/src/lib/shared-advert-card.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
+import EditIcon from '@material-ui/icons/Edit';
+import SaveIcon from '@material-ui/icons/Save';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import moment from 'moment';
 
 import './shared-advert-card.scss';
@@ -73,63 +76,153 @@ export const SharedAdvertCard = (props: SharedAdvertCardProps) => {
     return string;
   };
 
-  return (
-    <div className="advert-card">
-      <div className="card-title-wrap flex-wrap">
-        <button
-          type="button"
-          onClick={() => props.onViewClick(props.author._id)}
-        >
-          <img
-            className="card-img"
-            src={props.author.img}
-            alt=""
-            height="40"
-            width="40"
-          />
-        </button>
+  const [editing, setEditing] = useState(false);
 
-        <div className="card-title">
-          <a href="url" onClick={() => props.onViewClick(props.author._id)}>
-            <span>
-              {`
-								${props.author.firstName || ''} ${props.author.lastName || ''}
-							`}
-            </span>
-          </a>
+  const saveEdit = () => {
+    setEditing(false);
+  };
 
-          <span>posted on {props.title}</span>
-          <p>{`${timeCalculator(props.createAt)}`}</p>
+  if (!editing) {
+    return (
+      <div className="card">
+        <div className="card-title-wrap flex-wrap">
+          <button
+            type="button"
+            onClick={() => props.onViewClick(props.author._id)}
+          >
+            <img
+              className="card-img"
+              src={props.author.img}
+              alt=""
+              height="40"
+              width="40"
+            />
+          </button>
+
+          <div className="card-title">
+            <a href="url" onClick={() => props.onViewClick(props.author._id)}>
+              <span>
+                {`
+                  ${props.author.firstName || ''} ${props.author.lastName || ''}
+                `}
+              </span>
+            </a>
+            <span>posted on {props.title}</span>
+
+            <p>{`${timeCalculator(props.createAt)}`}</p>
+          </div>
+          <div className="editing-holder">
+            <EditIcon onClick={() => setEditing(true)} />
+          </div>
+        </div>
+
+        <div className="card-content">
+          <p>{props.description}</p>
+          <img src={props.imgUrl} alt="" />
+        </div>
+
+        <div className="social-wrapper">
+          <Button
+            variant="outlined"
+            color="secondary"
+            startIcon={<FavoriteIcon />}
+            onClick={props.onLikeClick}
+          >
+            <p>Like</p>
+            <div>{props.like}</div>
+          </Button>
+
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+            startIcon={<ShareIcon />}
+            onClick={props.onLikeClick}
+          >
+            <p>Share</p>
+            <div>{props.shared}</div>
+          </Button>
         </div>
       </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <form noValidate autoComplete="off">
+          <div className="card-title-wrap flex-wrap">
+            <button
+              type="button"
+              onClick={() => props.onViewClick(props.author._id)}
+            >
+              <img
+                className="card-img"
+                src={props.author.img}
+                alt=""
+                height="40"
+                width="40"
+              />
+            </button>
 
-      <div className="card-content">
-        <p>{props.description}</p>
-        <img src={props.imgUrl} alt="" />
+            <div className="card-title">
+              <a href="url" onClick={() => props.onViewClick(props.author._id)}>
+                <span>
+                  {`
+                    ${props.author.firstName || ''} ${
+                    props.author.lastName || ''
+                  }
+                  `}
+                </span>
+              </a>
+              <span>
+                posted on <TextField id="title" defaultValue={props.title} />
+              </span>
+
+              <p>{`${timeCalculator(props.createAt)}`}</p>
+            </div>
+            <div className="editing-holder editing">
+              <SaveIcon onClick={() => saveEdit()} />
+            </div>
+          </div>
+
+          <div className="card-content">
+            <TextField
+              id="description"
+              multiline
+              fullWidth
+              defaultValue={props.description}
+            />
+            <div className="image-container">
+              <img src={props.imgUrl} alt="" />
+              <div className="middle">
+                <div className="text">Upload</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="social-wrapper">
+            <Button
+              variant="outlined"
+              color="secondary"
+              startIcon={<FavoriteIcon />}
+              onClick={props.onLikeClick}
+            >
+              <p>Like</p>
+              <div>{props.like}</div>
+            </Button>
+
+            <Button
+              variant="outlined"
+              color="primary"
+              className={classes.button}
+              startIcon={<ShareIcon />}
+              onClick={props.onLikeClick}
+            >
+              <p>Share</p>
+              <div>{props.shared}</div>
+            </Button>
+          </div>
+        </form>
       </div>
-
-      <div className="social-wrapper">
-        <Button
-          variant="outlined"
-          color="secondary"
-          startIcon={<FavoriteIcon />}
-          onClick={props.onLikeClick}
-        >
-          <p>Like</p>
-          <div>{props.like}</div>
-        </Button>
-
-        <Button
-          variant="outlined"
-          color="primary"
-          className={classes.button}
-          startIcon={<ShareIcon />}
-          onClick={props.onLikeClick}
-        >
-          <p>Share</p>
-          <div>{props.shared}</div>
-        </Button>
-      </div>
-    </div>
-  );
+    );
+  }
 };
