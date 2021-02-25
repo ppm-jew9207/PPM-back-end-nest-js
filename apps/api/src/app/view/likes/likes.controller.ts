13a9b47d889540b72d0094e93966df10191c5c50
Lukas Ružauskas <nequadora@gmail.com>
2021-02-25 17:18:19 +0200

    removed unncessary things

--- apps/api/src/app/view/likes/likes.controller.ts
+++ apps/api/src/app/view/likes/likes.controller.ts
@@ -1,22 +1,9 @@
 import {
-  Controller,
-  Get,
-  Param,
-  UseInterceptors,
-  Req,
-  UseGuards,
+  Controller
 } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
-// import { GetLikesQuery } from './queries/handlers/get-likes.handler';
-// import { GetLikeQuery } from './queries/handlers/get-like.handler';
-// import { GetUsersLikeQuery } from './queries/handlers/get-users-like.handler';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { PrivateRoutesPath } from '@ppm/common/main';
-import { AuthGuard } from '@nestjs/passport';
-import { LoggingInterceptor } from '../../common/interceptors/logging.interceptor';
-import { TransformInterceptor } from '../../common/interceptors/transform.interceptor';
-import { LikesViewModel } from '../../models/Likes/Likes.interface';
-// import { GetUsersLikesQuery } from './queries/handlers/get-users-likes.handler';
 
 @Controller(PrivateRoutesPath.LIKES)
 @ApiTags(PrivateRoutesPath.LIKES)
