7b44adb919b9e591fdd362bb9e10611b5b001159
Lukas Ružauskas <nequadora@gmail.com>
2021-04-27 14:13:06 +0300

    endpoint to get mentors

--- apps/api/src/app/view/userProfiles/queries/index.ts
+++ apps/api/src/app/view/userProfiles/queries/index.ts
@@ -1,9 +1,11 @@
 import { GetUserProfileByUserIdHandler } from './get-user-profile-by-user-id.handler';
 import { GetUserProfileByIdHandler } from './get-user-profile-by-id.handler';
 import { GetUserProfileByEmailHandler } from './get-user-profile-by-email.handler';
+import { GetMentorsProfilesHandler } from './get-mentors-profiles.handler';
 
 export const QueryHandlers = [
   GetUserProfileByUserIdHandler,
   GetUserProfileByIdHandler,
   GetUserProfileByEmailHandler,
+  GetMentorsProfilesHandler,
 ];
--- apps/api/src/app/view/userProfiles/user-profiles.controller.ts
+++ apps/api/src/app/view/userProfiles/user-profiles.controller.ts
@@ -13,6 +13,7 @@ import { TransformInterceptor } from '../../common/interceptors/transform.interc
 import { QueryBus } from '@nestjs/cqrs';
 import { GetUserProfileByIdQuery } from './queries/get-user-profile-by-id.handler';
 import { GetUserProfileByEmailQuery } from './queries/get-user-profile-by-email.handler';
+import { GetMentorsProfilesQuery } from './queries/get-mentors-profiles.handler';
 import { UserProfilePayloadDto } from '../../models/userProfiles/dto/user-profile-payload.dto';
 import { GetUserProfileByUserIdQuery } from './queries/get-user-profile-by-user-id.handler';
 import { PrivateRoutesPath } from '@ppm/common/main';
@@ -31,6 +32,12 @@ export class UserProfilesController {
     const user = request.user;
     return this._queryBus.execute(new GetUserProfileByUserIdQuery(user.id));
   }
+
+  @Get('/mentors')
+  async getMentors() {
+    return this._queryBus.execute(new GetMentorsProfilesQuery());
+  }
+
   @Get(PrivateRoutesPath.GET_BY_ID)
   async getById(@Param('id') id: string): Promise<UserProfilePayloadDto> {
     return this._queryBus.execute(new GetUserProfileByIdQuery(id));
