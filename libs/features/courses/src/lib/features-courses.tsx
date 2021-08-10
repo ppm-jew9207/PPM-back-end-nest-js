403a72eddc1135e6a8c22b0f068965175d087c5c
edichkas <edichkas@yahoo.com>
2021-08-10 21:52:33 +0300

    added glidlist selector

--- libs/features/courses/src/lib/features-courses.tsx
+++ libs/features/courses/src/lib/features-courses.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { SharedCourseCard } from '@ppm/shared/course-card';
 import { useSelector, useDispatch } from 'react-redux';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -13,6 +13,8 @@ import {
 import './features-courses.scss';
 import { LikeEnum } from 'libs/data-access/likes/src/lib/types';
 import { useLocation } from 'react-router-dom';
+import { Button, ButtonGroup } from '@material-ui/core';
+import { Block, List, GridOn } from '@material-ui/icons';
 
 const stateSelector = createStructuredSelector({
   courses: coursesSelectors.selectCourses(),
@@ -24,6 +26,7 @@ export const FeaturesCourses = () => {
   const dispatch = useDispatch();
   const { courses, loading, profile } = useSelector(stateSelector);
   const [coursesState, setCoursesState] = useState([]);
+  const [viewState, setViewState] = useState('');
 
   const searchQuery = new URLSearchParams(useLocation().search).get('q');
 
@@ -59,48 +62,66 @@ export const FeaturesCourses = () => {
     dispatch(coursesActions.getAll(searchQuery));
   }, []);
 
+  const setListView = (viewOption: string) => {
+    setViewState(viewOption);
+  };
+
   if (loading) return <CircularProgress />;
 
   if (!coursesState) return <div className="no-items">No courses added...</div>;
   return (
-    <div className="course-cards">
-      {coursesState.length &&
-        coursesState.map((course, index) => (
-          <div key={course._id}>
-            <SharedCourseCard
-              id={course._id}
-              title={course.title}
-              author={{
-                _id: course.creator._id,
-                firstName: course.creator.name,
-                lastName: '',
-                img: course.creator.imageUrl,
-              }}
-              createAt={course.createdAt}
-              description={course.description}
-              like={
-                course.likesList
-                  ? course.likesList.filter(
-                      (like: any) => like.type === LikeEnum.Like
-                    ).length
-                  : 0
-              }
-              shared={
-                course.likesList
-                  ? course.likesList.filter(
-                      (like: any) => like.type === LikeEnum.Share
-                    ).length
-                  : 0
-              }
-              imgUrl={course.imageUrl}
-              onSaveClick={saveClick}
-              editable={profile?._id === course.creator._id}
-              onLikeClick={() => likeClick(course._id, LikeEnum.Like)}
-              onSharedClick={() => likeClick(course._id, LikeEnum.Share)}
-            />
-          </div>
-        ))}
-    </div>
+    <Fragment>
+      <ButtonGroup className="button-group">
+        <Button onClick={() => setListView('-list')} startIcon={<List />}>
+          Set List View
+        </Button>
+        <Button onClick={() => setListView('-grid')} startIcon={<GridOn />}>
+          Set Grid View
+        </Button>
+        <Button onClick={() => setListView('')} startIcon={<Block />}>
+          Set Tile View
+        </Button>
+      </ButtonGroup>
+
+      <div className={'course-cards' + viewState}>
+        {coursesState.length &&
+          coursesState.map((course, index) => (
+            <div key={course._id}>
+              <SharedCourseCard
+                id={course._id}
+                title={course.title}
+                author={{
+                  _id: course.creator._id,
+                  firstName: course.creator.name,
+                  lastName: '',
+                  img: course.creator.imageUrl,
+                }}
+                createAt={course.createdAt}
+                description={course.description}
+                like={
+                  course.likesList
+                    ? course.likesList.filter(
+                        (like: any) => like.type === LikeEnum.Like
+                      ).length
+                    : 0
+                }
+                shared={
+                  course.likesList
+                    ? course.likesList.filter(
+                        (like: any) => like.type === LikeEnum.Share
+                      ).length
+                    : 0
+                }
+                imgUrl={course.imageUrl}
+                onSaveClick={saveClick}
+                editable={profile?._id === course.creator._id}
+                onLikeClick={() => likeClick(course._id, LikeEnum.Like)}
+                onSharedClick={() => likeClick(course._id, LikeEnum.Share)}
+              />
+            </div>
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
