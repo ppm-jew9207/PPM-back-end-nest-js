d553644737d31153382de778651f71e805cc41fb
MatuzasM <matuzas2020@gmail.com>
2021-08-10 22:41:41 +0300

    moved static data to features component

--- libs/features/courses/src/lib/features-courses.tsx
+++ libs/features/courses/src/lib/features-courses.tsx
@@ -20,11 +20,67 @@ const stateSelector = createStructuredSelector({
   loading: coursesSelectors.selectLoading(),
   profile: userProfileSelectors.selectUserProfile(),
 });
-
+const filter = [
+  {
+    title: 'Ratings',
+    type: 'radio',
+    details: [
+      { label: '5 star', name: 'html' },
+      { label: '4 start', name: 'css' },
+      { label: '3 start', name: 'webdevelopment' },
+      { label: '2 start', name: 'react' },
+      { label: '1 start', name: 'javascript' },
+    ],
+  },
+  {
+    title: 'Topic',
+    type: 'checkbox',
+    details: [
+      { label: 'HTML', name: 'html' },
+      { label: 'CSS', name: 'css' },
+      { label: 'Web development', name: 'webdevelopment' },
+      { label: 'React.js', name: 'react' },
+      { label: 'Javascript', name: 'javascript' },
+    ],
+  },
+  {
+    title: 'Subcategory',
+    type: 'checkbox',
+    details: [
+      { label: 'Web development', name: 'text1' },
+      { label: 'Programming Languages', name: 'text1' },
+      { label: 'Game development', name: 'text1' },
+      { label: 'Web Design', name: 'text1' },
+      { label: 'Software Testing', name: 'text1' },
+    ],
+  },
+  {
+    title: 'Level',
+    type: 'checkbox',
+    details: [
+      { label: 'All Levels', name: 'text1' },
+      { label: 'Beginner', name: 'text1' },
+      { label: 'Intermediate', name: 'text1' },
+      { label: 'Expert', name: 'text1' },
+    ],
+  },
+  {
+    title: 'Video Duration',
+    type: 'checkbox',
+    details: [
+      { label: '0-1 hour', name: 'text1' },
+      { label: '1-3 hours', name: 'text1' },
+      { label: '3-6 hours', name: 'text1' },
+      { label: '6-10 hours', name: 'text1' },
+      { label: '10+ hours', name: 'text1' },
+    ],
+  },
+];
 export const FeaturesCourses = () => {
   const dispatch = useDispatch();
   const { courses, loading, profile } = useSelector(stateSelector);
   const [coursesState, setCoursesState] = useState([]);
+  const [filtersList, setFiltersList] = useState([]);
 
   const searchQuery = new URLSearchParams(useLocation().search).get('q');
 
@@ -53,6 +109,7 @@ export const FeaturesCourses = () => {
 
   useEffect(() => {
     setCoursesState(courses);
+    setFiltersList(filter);
   }, [courses]);
 
   useEffect(() => {
@@ -66,7 +123,15 @@ export const FeaturesCourses = () => {
   return (
     <div className="flex-card">
       <div className="filter-sidebar">
-        <SharedFilter />
+        {filtersList.length &&
+          filtersList.map((filterList, index) => (
+            <SharedFilter
+              key={index}
+              title={filterList.title}
+              type={filterList.type}
+              details={filterList.details}
+            />
+          ))}
       </div>
 
       <div className="course-cards">
--- libs/shared/filter/src/lib/shared-filter.tsx
+++ libs/shared/filter/src/lib/shared-filter.tsx
@@ -17,121 +17,52 @@ type Detail = {
   label: string;
   name: string;
 };
+
 // Defining an interface
 interface SharedFilterProps {
-  details: Detail[];
-  title: string;
-  type: string;
+  details?: Detail[];
+  title?: string;
+  type?: string;
 }
-// Type casting unto value
-const SharedFilterProps: SharedFilterProps[] = [
-  {
-    title: 'Ratings',
-    type: 'radio',
-    details: [
-      { label: '5 star', name: 'html' },
-      { label: '4 start', name: 'css' },
-      { label: '3 start', name: 'webdevelopment' },
-      { label: '2 start', name: 'react' },
-      { label: '1 start', name: 'javascript' },
-    ],
-  },
-  {
-    title: 'Topic',
-    type: 'checkbox',
-    details: [
-      { label: 'HTML', name: 'html' },
-      { label: 'CSS', name: 'css' },
-      { label: 'Web development', name: 'webdevelopment' },
-      { label: 'React.js', name: 'react' },
-      { label: 'Javascript', name: 'javascript' },
-    ],
-  },
-  {
-    title: 'Subcategory',
-    type: 'checkbox',
-    details: [
-      { label: 'Web development', name: 'text1' },
-      { label: 'Programming Languages', name: 'text1' },
-      { label: 'Game development', name: 'text1' },
-      { label: 'Web Design', name: 'text1' },
-      { label: 'Software Testing', name: 'text1' },
-    ],
-  },
-  {
-    title: 'Level',
-    type: 'checkbox',
-    details: [
-      { label: 'All Levels', name: 'text1' },
-      { label: 'Beginner', name: 'text1' },
-      { label: 'Intermediate', name: 'text1' },
-      { label: 'Expert', name: 'text1' },
-    ],
-  },
-  {
-    title: 'Video Duration',
-    type: 'checkbox',
-    details: [
-      { label: '0-1 hour', name: 'text1' },
-      { label: '1-3 hours', name: 'text1' },
-      { label: '3-6 hours', name: 'text1' },
-      { label: '6-10 hours', name: 'text1' },
-      { label: '10+ hours', name: 'text1' },
-    ],
-  },
-];
 
 export function SharedFilter(props: SharedFilterProps) {
   return (
     <form className="filter-form">
-      {SharedFilterProps.map((filter, i) => (
-        <Accordion key={i}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>{filter.title}</Typography>
-          </AccordionSummary>
-          {filter.details.map((detail, i) => (
-            <AccordionDetails key={i}>
-              {filter.type === 'checkbox' ? (
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{props.title}</Typography>
+        </AccordionSummary>
+        {props.details.map((detail, i) => (
+          <AccordionDetails key={i}>
+            {props.type === 'checkbox' ? (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={false}
+                    name={detail.label}
+                    color="primary"
+                  />
+                }
+                label={detail.label}
+              />
+            ) : (
+              <RadioGroup>
                 <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={false}
-                      name={detail.label}
-                      color="primary"
-                    />
-                  }
+                  value={detail.name}
+                  control={<Radio />}
                   label={detail.label}
                 />
-              ) : (
-                <RadioGroup>
-                  <FormControlLabel
-                    value={detail.name}
-                    control={<Radio />}
-                    label={detail.label}
-                  />
-                </RadioGroup>
-              )}
-            </AccordionDetails>
-          ))}
-        </Accordion>
-      ))}
+              </RadioGroup>
+            )}
+          </AccordionDetails>
+        ))}
+      </Accordion>
     </form>
   );
 }
 
 export default SharedFilter;
-
-// {
-//   props.filterItems.map((filterItem, i) => (
-//     <FormControlLabel
-//       control={
-//         <Checkbox checked={false} name={filterItem.name} color="primary" />
-//       }
-//       label={filterItem.label}
-//     />
-//   ));
-// }
