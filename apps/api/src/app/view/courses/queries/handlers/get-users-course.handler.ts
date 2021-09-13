1877bf89f7796ebe35dc543d28b456e3c8c9c1d0
Lukas Ružauskas <nequadora@gmail.com>
2021-09-13 21:45:40 +0300

    courses paging and counting

--- apps/api/src/app/view/courses/queries/handlers/get-users-course.handler.ts
+++ apps/api/src/app/view/courses/queries/handlers/get-users-course.handler.ts
@@ -4,14 +4,24 @@ import { Inject } from '@nestjs/common';
 import { CoursesViewModel } from '../../../../models/courses/courses.interface';
 
 export class GetUsersCourseQuery {
-  constructor(public readonly userId: string, public readonly id: string) {}
+  constructor(
+    public readonly userId: string,
+    public readonly id: string,
+    public readonly page?: string,
+    public readonly perPage?: string
+  ) {}
 }
 
 @QueryHandler(GetUsersCourseQuery)
 export class GetCoursesHandler implements IQueryHandler<GetUsersCourseQuery> {
   @Inject() private readonly _coursesViewService: CoursesModelService;
 
-  async execute({ userId, id }): Promise<CoursesViewModel> {
-    return this._coursesViewService.getUsersCourseById(userId, id);
+  async execute({ userId, id, page, perPage }): Promise<CoursesViewModel> {
+    return this._coursesViewService.getUsersCourseById(
+      userId,
+      id,
+      page,
+      perPage
+    );
   }
 }
