2c5d4fb2e459cd879ee2f6586fab9bd095bdcf81
MatuzasM <matuzas2020@gmail.com>
2021-11-03 14:05:41 +0200

    zero do not appear  while loading

--- libs/data-access/user-profile/src/lib/reducer.ts
+++ libs/data-access/user-profile/src/lib/reducer.ts
@@ -1,10 +1,11 @@
 import { ActionTypes } from './constants';
 import { ContainerState, ContainerActions } from './types';
+import { List, Map, fromJS } from 'immutable';
 
 export const initialState: ContainerState = {
   profile: null,
   loading: false,
-  loadedProfile: null
+  loadedProfile: null,
 };
 
 export function userProfileReducer(
@@ -16,38 +17,38 @@ export function userProfileReducer(
       return {
         profile: state.profile,
         loading: true,
-        loadedProfile: state.loadedProfile
+        loadedProfile: state.loadedProfile,
       };
     case ActionTypes.USER_PROFILE_GET_SUCCESS:
       return {
         profile: action.payload.profile,
         loading: false,
-        loadedProfile: state.loadedProfile
+        loadedProfile: state.loadedProfile,
       };
     case ActionTypes.USER_PROFILE_GET_FAILED:
       return {
         profile: state.profile,
         loading: false,
-        loadedProfile: state.loadedProfile
+        loadedProfile: state.loadedProfile,
       };
-      case ActionTypes.OTHER_USER_PROFILE_GET:
-        return {
-          profile: state.profile,
-          loading: true,
-          loadedProfile: state.loadedProfile
-        };
-      case ActionTypes.OTHER_USER_PROFILE_GET_SUCCESS:
-        return {
-          profile: state.profile,
-          loading: false,
-          loadedProfile: action.payload.loadedProfile
-        };
-      case ActionTypes.OTHER_USER_PROFILE_GET_FAILED:
-        return {
-          profile: state.profile,
-          loading: false,
-          loadedProfile: state.loadedProfile
-        };
+    case ActionTypes.OTHER_USER_PROFILE_GET:
+      return {
+        profile: state.profile,
+        loading: true,
+        loadedProfile: state.loadedProfile,
+      };
+    case ActionTypes.OTHER_USER_PROFILE_GET_SUCCESS:
+      return {
+        profile: state.profile,
+        loading: false,
+        loadedProfile: action.payload.loadedProfile,
+      };
+    case ActionTypes.OTHER_USER_PROFILE_GET_FAILED:
+      return {
+        profile: state.profile,
+        loading: false,
+        loadedProfile: state.loadedProfile,
+      };
     default:
       return state;
   }
